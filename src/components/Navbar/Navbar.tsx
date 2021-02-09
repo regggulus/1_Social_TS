f36f747c4c1ca434769ae0c5df75bcc5f33b3d96
Your Name <mikhalevichegor@gmail.com>
2021-02-09 20:53:31 +0300

    SocialNetwork-TS less20 NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css'
 
 export function Navbar() {
     return (
         <div className={s.nav}>
             <div className={s.item}>
-                <a href={'/profile'}>Profile</a>
-            </div>
-            <div className={`${s.item} + ${s.active}`}>
-                <a href={'/messages'}>Messages</a>
+                <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/news'}>News</a>
+                <NavLink to='/dialogs' activeClassName={s.active}>Dialogs</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/music'}>Music</a>
+                <NavLink to='/news' activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/settings'}>Settings</a>
+                <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
             </div>
         </div>
     )
