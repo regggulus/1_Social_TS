30a91a4f8d91840a986e61dfa1a35373371fa41f
Your Name <mikhalevichegor@gmail.com>
2021-02-09 23:05:09 +0300

    SocialNetwork-TS less21 added names

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,13 @@ import s from './Dialogs.module.css'
 export function Dialogs() {
     return (
         <div className={s.dialogs}>
-            Dialogs
+            <div className={s.dialogsItem}>
+                <div className={s.item}>Jonny</div>
+                <div className={s.item}>Josef</div>
+                <div className={s.item}>Steve</div>
+                <div className={s.item}>Bill</div>
+                <div className={s.item}>Nic</div>
+            </div>
         </div>
     )
 }
