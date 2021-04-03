b1fcb6d901f06a2ba073c62242cd4bfcff23195a
Egorych <mikhalevichegor@gmail.com>
2021-04-03 19:33:25 +0300

    SocialNetwork 43less container component (контейнерная компонента) создали контейнерную компоненту

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import s from './MyPosts.module.css'
+import {ActionsTypes, PostType} from "../../../redux/state";
+import {Post} from "./Post/Post";
+import {addPostAC, changeNewPostTextAC} from "../../../redux/profile-reducer";
+
+type MyPostsPropsType = {
+    posts: Array<PostType>
+    addPost: (addNewPost: string) => void
+    changeNewPostText: (newText: string) => void
+    newPostText: string
+    dispatch: (action: ActionsTypes) => void
+}
+
+
+export function MyPostsContainer(props: MyPostsPropsType) {
+
+    const newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    let addPost = () => {
+        if (newPostElement.current) {
+            props.dispatch(addPostAC(props.newPostText))
+            props.changeNewPostText('')
+        }
+    }
+    const onPostChange = () => {
+        if (newPostElement.current) {
+            const text = newPostElement.current.value
+            props.dispatch(changeNewPostTextAC(text))
+        }
+    }
+    return (
+        <div className={s.myPost}>
+            <h2>My post</h2>
+            <div>
+                <div>
+                    <textarea onChange={onPostChange}
+                              ref={newPostElement}
+                              value={props.newPostText}
+                              placeholder={'Enter your message'}
+                    >
+                    </textarea>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                <Post posts={props.posts}/>
+            </div>
+        </div>
+    )
+}
+
