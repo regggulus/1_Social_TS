948d299c00a4d7f9946e74c7f286cd5bccda41c6
Egorych <mikhalevichegor@gmail.com>
2021-05-11 00:16:32 +0300

    SocialNetwork. 56 - React JS практика - Презентационная и Контейнерная компоненты Part2.

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,7 +3,55 @@ import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unFollowAC, UsersType} from "../../redux/users-reducer";
 import {Dispatch} from "redux";
 import {RootStateType} from "../../redux/redux-store";
-import {UsersClassComponent} from "./UsersClassComponent";
+import axios from "axios";
+import {Users} from "./Users";
+
+type UsersPropsType = {
+    users: Array<UsersType>
+    pageSize: number
+    currentPage: number
+    totalUsersCount: number
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (users: Array<UsersType>) => void
+    setCurrentPage: (pageNumber: number) => void
+    setTotalUserCount: (totalUserCount: number) => void
+}
+
+export class UsersClassComponent extends React.Component<UsersPropsType> {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUserCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return (
+            <Users
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                pageSize={this.props.pageSize}
+                setUsers={this.props.setUsers}
+                currentPage={this.props.currentPage}
+                setCurrentPage={this.props.setCurrentPage}
+                totalUsersCount={this.props.totalUsersCount}
+                setTotalUserCount={this.props.setTotalUserCount}
+                onPageChanged={this.onPageChanged}
+            />
+        )
+    }
+}
 
 type MapStateToProps = {
     users: Array<UsersType>
